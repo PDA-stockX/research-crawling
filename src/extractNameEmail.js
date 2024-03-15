3458736118b13bc20a885e3134aec4f7a9f5dbc0
ye-s-rin <pyr53540@gmail.com>
2024-03-15 09:07:03 +0900

    이미지 데이터 문제: DS증권, 유안타증권

--- src/extractNameEmail.js
+++ src/extractNameEmail.js
@@ -16,24 +16,19 @@ function extractNameEmail(text) {
     let name = nameMatch ? nameMatch[1].replace(/\s/g, '') ? nameMatch[1].replace(/\s/g, '') : null : null;
 
     // 이름 추출을 위한 정규표현식
-    const nameRegex = /[ㄱ-ㅎㅏ-ㅣ가-힣]{3}/;
+    const nameRegex = /^(?!.*(팀장|대리|사원|선임|수석|연구원|연구위원))[ㄱ-ㅎㅏ-ㅣ가-힣]{3,4}$/;
 
-    // DFS를 사용하여 이메일 시작 인덱스를 기준으로 왼쪽으로 이동하며 이름을 찾기
-    function dfsLeft(index) {
+    // 이메일 시작 인덱스를 기준으로 왼쪽으로 이동하며 이름을 찾기
+    function shiftLeft(index) {
         if (index < 0 || name) return;
-        if (text.charAt(index) !== ' ') dfsLeft(index - 1);
-        else {
-            let word = '';
-            let i = index + 1;
-
-            if (name === null && i + 6 < text.length) {
-                word = text.substring(i, i + 6);
-                const wordMatch = word.replace(/\s/g, '').match(nameRegex);
-                name = wordMatch ? wordMatch[0] : null;
-            }
+        if (text.charAt(index) !== ' ') shiftLeft(index - 1);
+        else if (name === null && index + 7 < text.length) {
+            const word = text.substring(index + 1, index + 7).split(/\s{2,}/)[0];
+            const wordMatch = word.replace(/\s/g, '').match(nameRegex);
+            name = wordMatch ? wordMatch[0] : null;
 
             if (name === null) {
-                dfsLeft(index - 1);
+                shiftLeft(index - 1);
             }
         }
     }
@@ -41,8 +36,7 @@ function extractNameEmail(text) {
     if (email !== null && name === null) {
         const temp = text.indexOf(email);
         const emailIndex = temp !== -1 ? temp : text.indexOf('@');
-        // console.log("start dfs index:", emailIndex, ", ", text[emailIndex]);
-        dfsLeft(emailIndex - 1);
+        shiftLeft(emailIndex - 1);
     }
 
     return { name, email };
--- src/testExtract.js
+++ src/testExtract.js
@@ -18,21 +18,17 @@ function extractNameEmail(text) {
     let name = nameMatch ? nameMatch[1].replace(/\s/g, '') ? nameMatch[1].replace(/\s/g, '') : null : null;
 
     // 이름 추출을 위한 정규표현식
-    const nameRegex = /[ㄱ-ㅎㅏ-ㅣ가-힣]{3}/;
+    const nameRegex = /^(?!.*(팀장|대리|사원|선임|수석|연구원|연구위원))[ㄱ-ㅎㅏ-ㅣ가-힣]{3,4}$/;
 
     // 이메일 시작 인덱스를 기준으로 왼쪽으로 이동하며 이름을 찾기
     function shiftLeft(index) {
         if (index < 0 || name) return;
         if (text.charAt(index) !== ' ') shiftLeft(index - 1);
-        else {
-            let word = '';
-            let i = index + 1;
-
-            if (name === null && i + 6 < text.length) {
-                word = text.substring(i, i + 6);
-                const wordMatch = word.replace(/\s/g, '').match(nameRegex);
-                name = wordMatch ? wordMatch[0] : null;
-            }
+        else if (name === null && index + 7 < text.length) {
+            const word = text.substring(index + 1, index + 7).split(/\s{2,}/)[0];
+            const wordMatch = word.replace(/\s/g, '').match(nameRegex);
+            // console.log(wordMatch);
+            name = wordMatch ? wordMatch[0] : null;
 
             if (name === null) {
                 shiftLeft(index - 1);
@@ -58,6 +54,7 @@ function extractNameEmail(text) {
     const url5 = 'https://ssl.pstatic.net/imgstock/upload/research/company/1709857251930.pdf';
     const url6 = 'https://ssl.pstatic.net/imgstock/upload/research/company/1710113107594.pdf';
     const url7 = 'https://ssl.pstatic.net/imgstock/upload/research/company/1710112767446.pdf';
+    const url8 = 'https://ssl.pstatic.net/imgstock/upload/research/company/1709854066536.pdf';
 
     const text1 = await getContent(url1); //삼성증권
     const text2 = await getContent(url2); //유진투자증권
@@ -65,7 +62,8 @@ function extractNameEmail(text) {
     const text4 = await getContent(url4); //교보증권
     const text5 = await getContent(url5); //한화투자증권
     const text6 = await getContent(url6); //유안타증권 해결 X
-    const text7 = await getContent(url7); //심한투자증권 해결 X
+    const text7 = await getContent(url7); //심한투자증권 연구위원
+    const text8 = await getContent(url8); //심한투자증권 연구원 해결 X
 
     // console.log(text1);
     // console.log(extractNameEmail(text1));
@@ -77,8 +75,10 @@ function extractNameEmail(text) {
     // console.log(extractNameEmail(text4));
     // console.log(text5);
     // console.log(extractNameEmail(text5));
-    // console.log(text6);
-    // console.log(extractNameEmail(text6));
-    console.log(text7);
-    console.log(extractNameEmail(text7));
+    console.log(text6);
+    console.log(extractNameEmail(text6));
+    // console.log(text7);
+    // console.log(extractNameEmail(text7));
+    // console.log(text8);
+    // console.log(extractNameEmail(text8));
 })();
