83c10336a8e7ac52ee93dda85e44e5b0c426c351
ye-s-rin <pyr53540@gmail.com>
2024-03-20 10:55:37 +0900

    .

--- crawling/main.js
+++ crawling/main.js
@@ -1,10 +1,18 @@
-import reportList from "./reportList";
+import photoUrl from "./photoUrl.js";
+import reportList from "./reportList.js";
+import reportDetail from "./reportDetail.js";
+import stockDetail from "./stockDetail.js";
 
-const main = () => {
+const main = async () => {
+    // photoUrl();
+
     const start = new Date(new Date().setHours(23, 59, 0, 0));// 시작 날짜 (default: 오늘 23시 59분) - 포함
-    const end = new Date(new Date(Date.now() - 86400000).setHours(0, 0, 0, 0));// 끝 날짜 (default: 어제 0시 0분) - 제외
+    const end = new Date("2020-06-30T00:00:00");// 끝 날짜 (default: 어제 0시 0분) - 제외
+    // await reportList(start, end);
+    await reportList();
 
-    reportList(start, end);
+    reportDetail(start);
+    stockDetail(start);
 }
 
-// main();
+main();
