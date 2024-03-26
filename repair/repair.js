31f9f03d47b53850074627f0b2ad09416f79ff70
ye-s-rin <pyr53540@gmail.com>
2024-03-26 17:06:19 +0900

    nulUrls 처리 중

--- repair/repair.js
+++ repair/repair.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import pdfjs from '../src/pdfjs.js'
 import extractName from './extractName.js'
-import ocr from './useAdobeOcr.js'
+// import ocr from './useAdobeOcr.js'
 import extractNameEmail from '../src/extractNameEmail.js'
 
 import { str2date, date2str } from '../batch/date.js';
@@ -22,6 +22,8 @@ const repair = async (start) => {
     const resultPath = `../result/${dateStr}`
 
     const { Analyst, Report, nullUrls } = await getData(resultPath);
+    let problemFirmSet = new Set();
+    let problemFirms = [];
 
     const dirPath = './temp';
 
@@ -29,9 +31,12 @@ const repair = async (start) => {
         fs.mkdirSync(dirPath);
     }
 
+    console.log("nullUrls: ", nullUrls.length);
     for (const nullUrl of nullUrls) {
         const { i, pdfUrl } = nullUrl;
         const { name, firm, email, photoUrl } = Analyst[i];
+
+        problemFirms.push({ firm: Analyst[i].firm, pdfUrl });
 
         // console.log(i, pdfUrl, name, firm, email, photoUrl);
 
@@ -44,23 +49,34 @@ const repair = async (start) => {
                     Analyst[i].name = result.name;
                     Report[i].name = result.name;
                 });
+            problemFirmSet.add(Analyst[i].firm);
         }
         else if (ocrFirms.includes(firm)) {
             console.log("ocr version");
-            await ocr(pdfUrl)
-                .then((reportString) => extractNameEmail(reportString))
-                .then((result) => {
-                    console.log(result);
-                    Analyst[i].name = result.name;
-                    Report[i].name = result.name;
-                });
+            // await ocr(pdfUrl)
+            //     .then((reportString) => extractNameEmail(reportString))
+            //     .then((result) => {
+            //         console.log(result);
+            //         Analyst[i].name = result.name;
+            //         Report[i].name = result.name;
+            //     });
+            problemFirmSet.add(Analyst[i].firm);
         }
         else {
+            problemFirmSet.add(Analyst[i].firm);
             console.log("non covered")
         }
+        // console.log(Analyst[i]);
 
-        console.log(Analyst[i]);
+        fs.writeFileSync("./Analyst.json", JSON.stringify(Analyst));
+        fs.writeFileSync("./Report.json", JSON.stringify(Report));
+
+        fs.writeFileSync("./problemFirms.json", JSON.stringify(problemFirms));
+        fs.writeFileSync("./problemFirmSet.json", JSON.stringify([...problemFirmSet]));
     }
+
+    console.log(problemFirmSet);
+    console.log("countPdf: ", countPdf, "\tcountOcr: ", countOcr);
 };
 
-repair(new Date("2024-03-22T23:59:59"));
+repair(new Date("2024-03-20T23:59:59"));
