487821d4a80a413e4c8a1a8f7e6fd4cc94a47dca
ye-s-rin <pyr53540@gmail.com>
2024-03-27 14:00:31 +0900

    get photo image

--- crawling/downloadPhoto.js
+++ crawling/downloadPhoto.js
@@ -5,7 +5,7 @@ import { JSDOM } from 'jsdom';
 
 const downloadPhoto = async () => {
     const urls = JSON.parse(fs.readFileSync('../data/photoUrls.json'));
-    const dirPath = '../data/photo';
+    const dirPath = '../data/logos';
 
     if (!fs.existsSync(dirPath)) {
         fs.mkdirSync(dirPath);
@@ -22,65 +22,5 @@ const downloadPhoto = async () => {
         }
     }
 }
-
-const mmToPx = (mmValue, dpi = 96) => {
-    // 1 mm = dpi / 25.4 pixels
-    return mmValue * (dpi / 25.4);
-};
-
-const convertSvgSizeToPx = ({ width, height }) => {
-    const widthPx = width.endsWith('mm') ? mmToPx(parseFloat(width)) : parseFloat(width);
-    const heightPx = height.endsWith('mm') ? mmToPx(parseFloat(height)) : parseFloat(height);
-    return { width: `${widthPx}px`, height: `${heightPx}px` };
-};
-
-const getSvgSize = (svgContent) => {
-    const { window } = new JSDOM(svgContent);
-    const { document } = window;
-
-    const svgElement = document.querySelector('svg');
-    let width = svgElement.getAttribute('width');
-    let height = svgElement.getAttribute('height');
-
-    // SVG의 너비와 높이 정보가 없는 경우 viewBox 속성을 이용하여 실제 크기 추출
-    if (!width || !height) {
-        const viewBox = svgElement.getAttribute('viewBox');
-        if (viewBox) {
-            const viewBoxParts = viewBox.split(' ');
-            if (viewBoxParts.length === 4) {
-                width = viewBoxParts[2];
-                height = viewBoxParts[3];
-            } else {
-                // viewBox가 올바르지 않은 경우 기본값 설정
-                width = '1000px'; // 기본 너비
-                height = '300px'; // 기본 높이
-            }
-        } else {
-            // viewBox가 없는 경우 기본값 설정
-            width = '1000px'; // 기본 너비
-            height = '300px'; // 기본 높이
-        }
-    }
-
-    // 단위가 mm이면 px로 변환
-    if (width.endsWith('mm') || height.endsWith('mm')) {
-        const convertedSize = convertSvgSizeToPx({ width, height });
-        width = convertedSize.width;
-        height = convertedSize.height;
-    }
-
-    return { width, height };
-};
-
-const convertSvgToPng = async (svgContent, pngFilePath, { width, height }) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.setViewport({ width: parseInt(width), height: parseInt(height) });
-    await page.setContent(svgContent);
-    await page.screenshot({ path: pngFilePath });
-
-    await browser.close();
-};
 
 downloadPhoto();
